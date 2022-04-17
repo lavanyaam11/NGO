ec84a59603e23d96f21dd1a282cfbe3856e92fda
Lavanya <lavanyaam77@gmai.com>
2022-04-17 16:29:22 +0530

    Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+    return (
+
+        <footer className="footer">
+
+            <div className="footer-copyright text-center">© 2022 Copyright:
+            </div>
+
+        </footer>
+
+    )
+}
